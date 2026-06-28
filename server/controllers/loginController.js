c1ef651acad72f573434a33b2cfb01d6c90a4ad3
krishnaking00480-stack <krishnaking00480@gmail.com>
2026-06-28 20:15:06 +0530

    Add login debug logs

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,6 +2,13 @@ const login = (req, res) => {
   const username = req.body.username.trim();
   const password = req.body.password.trim();
 
+  console.log("================================");
+  console.log("Entered Username:", `"${username}"`);
+  console.log("Entered Password:", `"${password}"`);
+  console.log("ENV Username:", `"${process.env.ADMIN_USERNAME}"`);
+  console.log("ENV Password:", `"${process.env.ADMIN_PASSWORD}"`);
+  console.log("================================");
+
   if (
     username === process.env.ADMIN_USERNAME.trim() &&
     password === process.env.ADMIN_PASSWORD.trim()
